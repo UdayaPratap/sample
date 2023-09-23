ff445b2836a290222d62ed77185012c2feff4306
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-09-23 22:14:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.proto.Widget_pb2 import WidgetState
 
 # Define fixed commands and responses for your chatbot
 chatbot_responses = {
@@ -8,11 +9,18 @@ chatbot_responses = {
     "bye": "Goodbye! Have a great day!"
 }
 
+# Function to initialize the conversation state
+def init_conversation():
+    return []
+
+# Function to get the current conversation
+def get_conversation():
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = init_conversation()
+    return st.session_state.conversation
+
 # Streamlit app header
 st.title("Simple Chatbot")
-
-# Initialize conversation history
-conversation = []
 
 # Instructions
 st.sidebar.markdown("## Instructions")
@@ -20,40 +28,22 @@ st.sidebar.write("1. Type your message in the text input box.")
 st.sidebar.write("2. Click the 'Send' button to chat with the bot.")
 st.sidebar.write("3. Type 'exit' to end the conversation.")
 
-widget_counter = 1
+# Initialize conversation
+conversation = get_conversation()
 
-def chat_widget(widget_counter):
-    # User input
-    user_input = st.text_input("You:", key=f"chat_input_{widget_counter}")
-    return user_input
+# User input
+user_input = st.text_input("You:")
 
-# Bot response and conversation loop
-def chat_loop(widget_counter):
-    user_input = chat_widget(widget_counter)
-    
-    while user_input.lower() != "exit":
-        submit_button_key = f"submit_button_{widget_counter}"
-        submit_button = st.button("Send", key=submit_button_key)
+# Submit button
+if st.button("Send"):
+    user_input = user_input.lower()
+    conversation.append(f"You: {user_input}")
 
-        if submit_button:
-            user_input = user_input.lower()
-            conversation.append(f"You: {user_input}")
-
-            response_container = st.empty()
-            
-            if user_input in chatbot_responses:
-                response = chatbot_responses[user_input]
-                conversation.append(f"Bot: {response}")
-                response_container.write(f"Bot: {response}")
-            else:
-                conversation.append("Bot: I'm sorry, I don't understand that.")
-                response_container.write("Bot: I'm sorry, I don't understand that.")
-
-            widget_counter += 1
-            user_input = chat_widget(widget_counter)
-
-# Call the chat loop function to start the conversation
-chat_loop(widget_counter)
+    if user_input in chatbot_responses:
+        response = chatbot_responses[user_input]
+        conversation.append(f"Bot: {response}")
+    else:
+        conversation.append("Bot: I'm sorry, I don't understand that.")
 
 # Display conversation history
 st.text_area("Conversation History", value="\n".join(conversation), key="conversation_history")
