48d3d0d7430ea8ee06a02744d1b1c483651dc87d
Udaya Pratap Singh <121041778+UdayaPratap@users.noreply.github.com>
2023-09-24 16:48:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,18 +47,18 @@
 # # Display conversation history
 # st.text_area("Conversation History", value="\n".join(conversation), key="conversation_history")
 
-    import streamlit as st
-    scraped_data={
-          'title': "a",
-          'price': "b",
-          'description': "c",
-          'rating':"d",
-          'reviews_count': "e",
-          'availability': "f"
-    }
-    st.subheader("Chat with SHopy - Your Shopping Assistant")
+import streamlit as st
+scraped_data={
+        'title': "a",
+        'price': "b",
+        'description': "c",
+        'rating':"d",
+        'reviews_count': "e",
+        'availability': "f"
+}
+st.subheader("Chat with SHopy - Your Shopping Assistant")
 
-    chatbot_responses = {
+ chatbot_responses = {
         'title': ['name', 'title', 'brand', 'product name', 'what is it called'],
         'price': ['price', 'cost', 'how much', 'value', 'worth', 'expense'],
         'description': ['short description', 'brief description', 'describe', 'details', 'detail', 'specs', 'specifications', 'features'],
@@ -66,39 +66,39 @@
         'reviews_count': ['reviews count', 'no of reviews', 'number of reviews', 'how many reviews'],
         'availability': ['deliver', 'delivery', 'available', 'availability', 'in stock', 'can I buy it'],
         'all_info': ['display all data', 'display all info', 'display all information', 'give all info', 'show everything', 'show all details']
-    }
+}
 
     # Function to initialize the conversation state
-    def init_conversation():
-        return []
+def init_conversation():
+    return []
     # Function to get the current conversation
-    def get_conversation():
-        if "conversation" not in st.session_state:
-            st.session_state.conversation = init_conversation()
-        return st.session_state.conversation
-    # Initialize conversation
-    conversation = get_conversation()
-    # User input
-    user_input = st.text_input("You:")
+def get_conversation():
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = init_conversation()
+    return st.session_state.conversation
+# Initialize conversation
+conversation = get_conversation()
+# User input
+user_input = st.text_input("You:")
     
     # Submit button
-    if st.button("Send"):
-        user_input = user_input.lower()
-        conversation.append(f"You: {user_input}")
-        found_match = False
-        for key, synonyms in chatbot_responses.items():
-            for synonym in synonyms:
-                if synonym in user_input.lower():
-                    if key=='all_info':
-                        response=f"Here are all the details of the product:\n{chatbot_responses}"
-                        found_match=True
-                    else:
-                        response=f"The {key} of the product is: {str(chatbot_responses[key])}"
-                        found_match=True
+if st.button("Send"):
+    user_input = user_input.lower()
+    conversation.append(f"You: {user_input}")
+    found_match = False
+    for key, synonyms in chatbot_responses.items():
+        for synonym in synonyms:
+            if synonym in user_input.lower():
+                if key=='all_info':
+                    response=f"Here are all the details of the product:\n{chatbot_responses}"
+                    found_match=True
+                else:
+                    response=f"The {key} of the product is: {str(chatbot_responses[key])}"
+                    found_match=True
                     
-        if not found_match:
-            response = "I'm sorry, I didn't understand your question. Could you please rephrase it?"
-        conversation.append(response)
+    if not found_match:
+        response = "I'm sorry, I didn't understand your question. Could you please rephrase it?"
+    conversation.append(response)
 
-    # Display conversation history
-    st.text_area("Conversation History", value="\n".join(conversation), key="conversation_history")
+# Display conversation history
+st.text_area("Conversation History", value="\n".join(conversation), key="conversation_history")
